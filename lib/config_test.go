0ce4c41e2dc1c270179663a4063764eccb971c26
Buddhike de Silva <buddyspike@geeksdiary.com>
2020-07-29 21:24:06 +1000

    Fixed tests

--- lib/config_test.go
+++ lib/config_test.go
@@ -12,27 +12,28 @@ func Now() time.Time {
 }
 
 func TestParsingDuration(t *testing.T) {
-	c := &Config{"a", "a", "1m", ""}
+	c := &Config{Rewind: "1m"}
+
 	r, err := c.CalculatePointInTime(Now)
 	assert.NoError(t, err)
 	assert.Equal(t, r, time.Date(2020, 1, 2, 7, 59, 0, 0, time.Local))
 }
 
 func TestParsingInvalidDuration(t *testing.T) {
-	c := &Config{"a", "a", "1msfd", ""}
+	c := &Config{Rewind: "1msfd"}
 	_, err := c.CalculatePointInTime(Now)
 	assert.Error(t, err, "time: unknown unit msfd in duration 1msfd")
 }
 
 func TestParsingSince(t *testing.T) {
-	c := &Config{"a", "a", "", "2020-01-02T07:00:00+11:00"}
+	c := &Config{Since: "2020-01-02T07:00:00+11:00"}
 	r, err := c.CalculatePointInTime(Now)
 	assert.NoError(t, err)
 	assert.Equal(t, r, time.Date(2020, 1, 2, 7, 0, 0, 0, time.Local))
 }
 
 func TestParsingInvalidSince(t *testing.T) {
-	c := &Config{"a", "a", "", "2020-01-02"}
+	c := &Config{Since: "2020-01-02"}
 	_, err := c.CalculatePointInTime(Now)
 	assert.EqualError(t, err, "unable to recognise the specified time")
 }
